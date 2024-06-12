c18b053a09ea707a28f05684c44e8718a2927a32
revestskiy <137896160+revestskiy@users.noreply.github.com>
2024-06-12 12:40:28 +0300

    Add files via upload

---
+++ 5dz/Program.cs
@@ -0,0 +1,17 @@
+using _5pks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
