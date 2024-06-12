860e59bef16617f12e10c991c46c7d6b97391bbb
revestskiy <137896160+revestskiy@users.noreply.github.com>
2024-06-12 12:44:29 +0300

    Add files via upload

---
+++ 5dz/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using _5pks.models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace _5pks.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = _context.Students.FirstOrDefault(s => s.Email == model.Email && s.Password == model.Password);
+                if (student != null)
+                {
+                    // Успешная авторизация, перенаправление в личный кабинет
+                    return RedirectToAction("Index", "Student");
+                }
+
+                // Ошибка авторизации
+                ModelState.AddModelError("", "Неверный email или пароль");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ 5dz/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using _5pks.models;
+using System.Collections.Generic;
+namespace _5pks.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Performance()
+        {
+            var performances = _context.Performances.ToList();
+            return View(performances);
+        }
+
+        public IActionResult Achievements(string filterType, DateTime? filterDate)
+        {
+            var achievements = _context.Achievements.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filterType))
+            {
+                achievements = achievements.Where(a => a.Type == filterType);
+            }
+
+            if (filterDate.HasValue)
+            {
+                achievements = achievements.Where(a => a.Date.Date == filterDate.Value.Date);
+            }
+
+            return View(achievements.ToList());
+        }
+
+        public IActionResult Teachers()
+        {
+            var teachers = _context.Teachers.ToList();
+            return View(teachers);
+        }
+    }
+}
